9c3b4feaf4e5ff9e83919c95d1e2abfe15de0305
Daniel Souza <daniel.n10@hotmail.com>
2020-09-11 17:01:17 -0300

    criando a relação entre as tabelas

--- src/database/migrations/1599760394985-CreateAppointments.ts
+++ src/database/migrations/1599760394985-CreateAppointments.ts
@@ -8,7 +8,7 @@ export default class CreateAppointments1599760394985 implements MigrationInterfa
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()'
--- src/database/migrations/1599834171736-CreateUsers.ts
+++ src/database/migrations/1599834171736-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1599834171736 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
