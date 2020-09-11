7b3373141dcdacc18cc40863ba037c410a61f2c0
Daniel Souza <daniel.n10@hotmail.com>
2020-09-11 10:58:00 -0300

    configuração do eslint, prettier e do typeorm

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,15 @@
+import 'reflect-metadata';
+
 import express from 'express';
+import routes from './routes';
+
+import './database';
 
 const app = express();
 
-app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World!'})
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(3333, () => {
-    console.log('Server started on por 3333!')
+    console.log('Server started on port 3333!')
 });
