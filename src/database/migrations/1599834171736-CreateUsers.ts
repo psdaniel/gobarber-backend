e26a9988e939928e14d67c290874335be5f122e3
Daniel Souza <daniel.n10@hotmail.com>
2020-09-11 13:56:47 -0300

    modificação tabela appointments e criação tabela users

--- src/database/migrations/1599834171736-CreateUsers.ts
+++ src/database/migrations/1599834171736-CreateUsers.ts
@@ -1,27 +1,30 @@
-import {MigrationInterface, QueryRunner, Table } from "typeorm";
+import {MigrationInterface, QueryRunner, Table} from "typeorm";
 
-export default class CreateAppointments1599760394985 implements MigrationInterface {
+export default class CreateUsers1599834171736 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.createTable(
             new Table({
-                name: 'appointments',
+                name: 'users',
                 columns: [
                     {
                         name: 'id',
                         type: 'varchar',
                         isPrimary: true,
                         generationStrategy: 'uuid',
-                        default: 'uuid_generate_v4()'
+                        default: 'uuid_generate_v4()',
                     },
                     {
-                        name: 'provider',
+                        name: 'name',
                         type: 'varchar',
-                        isNullable: false,
                     },
                     {
-                        name: 'date',
-                        type: 'timestamp with time zone',
-                        isNullable: false
+                        name: 'email',
+                        type: 'varchar',
+                        isUnique: true,
+                    },
+                    {
+                        name: 'password',
+                        type: 'varchar',
                     },
                     {
                         name: 'created_at',
@@ -33,13 +36,13 @@ export default class CreateAppointments1599760394985 implements MigrationInterfa
                         type: 'timestamp',
                         default: 'now()',
                     },
-                ]
-            })
-        )
+                ],
+            }),
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.dropTable('appointments');
+        await queryRunner.dropTable('users')
     }
 
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,22 +1,24 @@
 import { Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
 
-@Entity('appointments')
-class Appointment {
+@Entity('users')
+class User {
     @PrimaryGeneratedColumn('uuid')
     id: string;
 
     @Column()
-    provider: string;
+    name: string;
 
-    @Column('timestamp with time zone')
-    date: Date;
+    @Column()
+    email: string;
+
+    @Column()
+    password: string;
 
     @CreateDateColumn()
     created_at: Date;
 
     @UpdateDateColumn()
     updated_at: Date;
-
 }
 
-export default Appointment;
+export default User;
