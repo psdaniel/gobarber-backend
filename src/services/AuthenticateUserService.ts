e64be9eab4fc3d096364787358dff85fec6ca2f4
Daniel Souza <daniel.n10@hotmail.com>
2020-09-12 17:33:48 -0300

    geração do token jwt

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign, verify } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
     user: User;
+    token: string;
 }
 
 class AuthenticateUserService {
@@ -28,8 +30,14 @@ class AuthenticateUserService {
             throw new Error('Incorret email/password combination.')
         }
 
+        const token = sign({}, '4bf153a6a5b7a1e6f52ad79e1a36f567', {
+            subject: user.id,
+            expiresIn: '1d',
+        });
+
         return {
             user,
+            token,
         };
     }
 }
