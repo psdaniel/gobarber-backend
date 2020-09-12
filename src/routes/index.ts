d4ddae683747b2ad4ff16a92cbca0b93f407c643
Daniel Souza <daniel.n10@hotmail.com>
2020-09-12 15:26:25 -0300

    criação de registros

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
 
+import appointmentsRouter from './appointments.routes';
+import usersRouter from './users.routes';
+
+
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({message: 'Hello GoStack'}));
+routes.use('/appointments', appointmentsRouter);
+routes.use('/users', usersRouter);
+
 
 export default routes;
--- src/server.ts
+++ src/server.ts
@@ -8,7 +8,6 @@ import './database';
 const app = express();
 
 app.use(express.json());
-app.use(routes);
 
 app.listen(3333, () => {
     console.log('Server started on port 3333!')
