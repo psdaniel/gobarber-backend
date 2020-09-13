5eb15d585c2ff2c36ae472daf98d2de4e6acc821
Daniel Souza <daniel.n10@hotmail.com>
2020-09-13 10:42:40 -0300

    configuração do upload de arquivos

---
+++ src/database/migrations/1599952866725-AddAvatarFieldToUsers.ts
@@ -0,0 +1,16 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export class AddAvatarFieldToUsers1599952866725 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('users', new TableColumn({
+            name: 'avatar',
+            type: 'varchar',
+            isNullable: true,
+        }),
+        );
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('users', 'avatar')
+    }
+}
