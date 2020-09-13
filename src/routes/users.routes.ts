b61e49288d4b704802226b33c4625332bad5f25b
Daniel Souza <daniel.n10@hotmail.com>
2020-09-13 19:22:55 -0300

    tratativa global de erros

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -11,36 +11,28 @@ const usersRouter = Router();
 const upload = multer(uploadConfig);
 
 usersRouter.post('/', async (request, response) => {
-    try {
-        const { name, email, password } = request.body;
+    const { name, email, password } = request.body;
 
-        const createUser = new CreateUserService();
+    const createUser = new CreateUserService();
 
-        const user = await createUser.execute({
-            name,
-            email,
-            password,
-        });
+    const user = await createUser.execute({
+        name,
+        email,
+        password,
+    });
 
-        return response.json(user)
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
+    return response.json(user)
 });
 
 usersRouter.patch('/avatar', ensureAuthenticated, upload.single('avatar'), async (request, response) => {
-    try {
-        const updateUserAvatar = new UpdateUserAvatarService();
+    const updateUserAvatar = new UpdateUserAvatarService();
 
-        const user = await updateUserAvatar.execute({
-            user_id: request.user.id,
-            avatarFilename: request.file.filename,
-        });
+    const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFilename: request.file.filename,
+    });
 
-        return response.json(user);
-    } catch (err) {
-        return response.status(400).json({ error: err.message });
-    }
-})
+    return response.json(user);
+});
 
 export default usersRouter;
