77a5b911a835a3131f063e3fa1b322930ef6c7aa
Daniel Souza <daniel.n10@hotmail.com>
2020-09-24 20:00:47 -0300

    finalização da funcionalidade de recuperação e reset de senha

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -32,9 +32,9 @@ class SendForgotPasswordEmailService {
             throw new AppError('User does not exists.')
         }
 
-        await this.userTokensRepository.generate(user.id)
+        const { token } = await this.userTokensRepository.generate(user.id)
 
-        this.mailProvider.sendMail(email, 'Pedido de recuperação de senha recebido.')
+        await this.mailProvider.sendMail(email, `Pedido de recuperação de senha recebido. ${token}`)
     }
 }
 
