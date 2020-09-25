30dd55eb2c92b67125b88458fe3871dd8847cb31
Daniel Souza <daniel.n10@hotmail.com>
2020-09-25 10:56:40 -0300

    configurando a estrutura do template de e-mails

--- src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
+++ src/shared/container/providers/MailProvider/fakes/FakeMailProvider.ts
@@ -1,18 +1,10 @@
 import IMailProvider from '../models/IMailProvider';
-
-interface Message {
-    to: string;
-    body: string;
-}
+import ISendMailDTO from '../dtos/ISendMailDTO';
 
 export default class FakeMailProvider implements IMailProvider {
-    private messages: Message[] = [];
+    private messages: ISendMailDTO[] = [];
 
-    public async sendMail(to: string, body: string): Promise<void> {
-        this.messages.push({
-            to,
-            body,
-        })
-
+    public async sendMail(message: ISendMailDTO): Promise<void> {
+        this.messages.push(message);
     }
 }
