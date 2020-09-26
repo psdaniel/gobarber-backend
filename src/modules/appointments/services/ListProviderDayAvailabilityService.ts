e97c2e61d17c84a962627de8935949781d246f2c
Daniel Souza <daniel.n10@hotmail.com>
2020-09-26 15:36:49 -0300

    listagem dos horários disponíveis

---
+++ src/modules/appointments/services/ListProviderDayAvailabilityService.ts
@@ -0,0 +1,53 @@
+import { injectable, inject } from 'tsyringe';
+import { getHours } from 'date-fns';
+
+import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+
+//import User from '@modules/users/infra/typeorm/entities/User';
+
+interface Request {
+    provider_id: string;
+    day: number;
+    month: number;
+    year: number;
+}
+
+type Response = Array<{
+    hour: number;
+    available: boolean;
+}>;
+
+@injectable()
+class ListProviderDayAvailabilityService {
+    constructor(
+        @inject('AppointmentsRepository')
+        private appointmentsRepository: IAppointmentsRepository,
+    ) {}
+
+    public async execute({ provider_id, day, year, month }: Request): Promise<Response> {
+        const appointments = await this.appointmentsRepository.findAllInDayFromProvider({
+            provider_id,
+            day,
+            year,
+            month
+        });
+
+        const hourStart = 8;
+
+        const eachHourArray = Array.from({ length: 10 }, (_, index) => index + hourStart,
+        );
+
+        const availability = eachHourArray.map(hour => {
+            const hasAppointmentInHour = appointments.find(appointment => getHours(appointment.date) === hour, );
+
+            return {
+                hour,
+                available: !hasAppointmentInHour,
+            }
+        });
+
+        return availability;
+    }
+}
+
+export default ListProviderDayAvailabilityService;
--- src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -23,7 +23,47 @@ describe('ListProviderMonthAvailability', () => {
 
         await fakeAppointmentsRepository.create({
             provider_id: 'user',
+            date: new Date(2020, 4, 20, 9, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
             date: new Date(2020, 4, 20, 10, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 11, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 12, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 13, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 14, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 15, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 16, 0, 0),
+        });
+
+        await fakeAppointmentsRepository.create({
+            provider_id: 'user',
+            date: new Date(2020, 4, 20, 17, 0, 0),
         });
 
         await fakeAppointmentsRepository.create({
@@ -40,7 +80,7 @@ describe('ListProviderMonthAvailability', () => {
         expect(availability).toEqual(expect.arrayContaining([
             { day: 19, available: true },
             { day: 20, available: false },
-            { day: 21, available: false },
+            { day: 21, available: true },
             { day: 22, available: true },
         ]))
     });
