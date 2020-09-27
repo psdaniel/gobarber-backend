853687d375bf6fb6aba80d4e0cc13b808d7a1eb6
Daniel Souza <daniel.n10@hotmail.com>
2020-09-27 20:37:01 -0300

    envio de notificações

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,10 +1,12 @@
-import { startOfHour, isBefore, getHours } from 'date-fns';
+import { startOfHour, isBefore, getHours, format } from 'date-fns';
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
 
 import Appointment from '../infra/typeorm/entities/Appointment';
 import IAppointmentsRepository from '../repositories/IAppointmentsRepository';
+import INotificationsRepository from '@modules/notification/repositories/INotificationsRepository';
+
 
 
 interface IRequest {
@@ -18,6 +20,9 @@ class CreateAppointmentService {
     constructor(
         @inject('AppointmentsRepository')
         private appointmentsRepository: IAppointmentsRepository,
+
+        @inject('NotificationsRepository')
+        private notificationsRepository: INotificationsRepository,
         ) {
 
     }
@@ -50,6 +55,13 @@ class CreateAppointmentService {
             user_id,
             date: appointmentDate,
         });
+
+        const formattedDate = format(appointmentDate, "dd/MM/yyyy 'às' HH:mm'h'")
+
+        await this.notificationsRepository.create({
+            recipient_id: provider_id,
+            content: `Novo agendamento para o dia ${formattedDate}`,
+        })
 
         return appointment;
     }
